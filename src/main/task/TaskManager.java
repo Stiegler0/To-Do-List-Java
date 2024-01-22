3930736944c1537cdafdd4857f4d0d1caa56745e
Stiegler0 <yassinejemlaoui23@gmail.com>
2024-01-22 14:59:25 +0100

    bug fixed, I added break in switch statement after finding a matching case, in this way, the programm will not continue to the next case

--- src/main/task/TaskManager.java
+++ src/main/task/TaskManager.java
@@ -4,15 +4,24 @@ import java.util.*;
 // classe pour gérer les opérations: ajout, ....
 public class TaskManager {
     //ndiro array list tant que attribut de classe 7sn ma variable local f method
-    private List<Task> taskList = new ArrayList<>();
+    private ArrayList<Task> taskList = new ArrayList<>();
     public void addtask(int id, String name, String description){
         // add method in ArrayList class:
         //taskList.add(Task.ob);
         Task newtask = new Task(id, name, description);
         taskList.add(newtask);
+        System.out.print("Tâche ajoutée: ");
+        if (taskList.isEmpty())
+            System.out.println("Liste vide");
+        else{
+            System.out.println(newtask);
+        }
+
+
     }
     public void displaytask(int taskindex){
         Task selectedTask = taskList.get(taskindex);
         System.out.println(selectedTask.gettitle());
+        //compare the id, loop through the array list and then display the convenable task
     }
 }
