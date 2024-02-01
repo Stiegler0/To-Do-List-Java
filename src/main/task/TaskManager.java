9723733edfed1bcc98ace44faee5c248ceae3627
Stiegler0 <yassinejemlaoui23@gmail.com>
2024-02-02 00:01:03 +0100

    serialization

--- src/main/task/TaskManager.java
+++ src/main/task/TaskManager.java
@@ -23,20 +23,36 @@ public class TaskManager {
                 System.out.println("---------------");
             }
         }
+        serializeTaskList();
+    }
+    public void serializeTaskList(){
         try{
             FileOutputStream fileout = new FileOutputStream("task.ser");
             ObjectOutputStream out = new ObjectOutputStream(fileout);
-            out.writeObject(newtask);
+            out.writeObject(taskList);
             out.close();
             fileout.close();
             System.out.printf("Serialization is done");
         }catch (IOException i){
             i.printStackTrace();
         }
-
-
-
     }
+    public List<Task> loadTasks(String filename){
+        List<Task> tasks = null;
+        try{
+            FileInputStream fileIn = new FileInputStream(filename);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            tasks = (ArrayList<Task>) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return tasks;
+    }
+
     public void displaytask(int taskid){
         // provide code to display the task
 
@@ -53,33 +69,26 @@ public class TaskManager {
 
         //compare the id, loop through the array list and then display the convenable task
     }
-    public List<Task> loadTasks(String filename){
-        List<Task> tasks = null;
-        try{
-            FileInputStream fileIn = new FileInputStream(filename);
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            tasks = (List<Task>) in.readObject();
-            in.close();
-            fileIn.close();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-    }
-        return tasks;
-    }
+
 
     public void displayAllnonCompletedTasks(){
+
         taskList = (ArrayList<Task>) loadTasks("task.ser");
-        for (Task tak: taskList){
-            if(tak.getCompleted() == false){
-                System.out.println("ID: " + tak.getID());
-                System.out.println("Titre: " + tak.getTitle());
-                System.out.println("Description: " + tak.getDescription());
-                System.out.println("Status: " + tak.getCompletionStatus());
-                System.out.println("---------------");
+        if (taskList != null){
+            for (Task tak: taskList){
+                if(tak.getCompleted() == false){
+                    System.out.println("ID: " + tak.getID());
+                    System.out.println("Titre: " + tak.getTitle());
+                    System.out.println("Description: " + tak.getDescription());
+                    System.out.println("Status: " + tak.getCompletionStatus());
+                    System.out.println("---------------");
+                }
             }
+        }else{
+            System.out.println("Liste vide");
         }
+
+
     }
 
     public void checkstatusofTask(int id){
